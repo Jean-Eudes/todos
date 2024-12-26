fef9841e0fef3726c29117fd65c74e8cf5442563
jean-eudes <jeudes7@gmail.com>
2024-12-26 15:31:44 +0100

    use pool

--- src/main.rs
+++ src/main.rs
@@ -6,23 +6,36 @@
 mod model;
 mod schema;
 
+use crate::model::TodosToPersist;
 use axum::http::StatusCode;
 use axum::routing::{get, post};
-use axum::{Json, Router};
-use diesel::Connection;
+use axum::{debug_handler, Json, Router};
+use diesel::r2d2::ConnectionManager;
 use diesel::PgConnection;
+use diesel::{r2d2, Connection};
 use std::env;
-use crate::model::TodosToPersist;
+use axum::extract::State;
+
+#[derive(Clone)]
+struct AppState {
+    pool: r2d2::Pool<ConnectionManager<PgConnection>>,
+}
 
 #[tokio::main]
 async fn main() {
     let database_url = "postgres://omc_projet:omc_projet@localhost:5432/todos";
-    PgConnection::establish(&database_url)
-        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url));
+    let manager = ConnectionManager::<PgConnection>::new(database_url);
+    let pool = r2d2::Pool::builder()
+        .build(manager)
+        .expect("Failed to create pool.");
+
+    let state = AppState { pool };
+
     // build our application with a route
     let app = Router::new()
         .route("/", get(handler2))
-        .route("/", post(handler));
+        .route("/", post(handler))
+        .with_state(state);
     // run it
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
@@ -32,14 +45,15 @@ async fn main() {
 }
 
 async fn handler(title: String) -> StatusCode {
-
     StatusCode::CREATED
 }
-async fn handler2() -> Json<Vec<TodosToPersist>> {
-    let database_url = "postgres://omc_projet:omc_projet@localhost:5432/todos";
-    let mut connection = PgConnection::establish(&database_url)
-        .unwrap_or_else(|_| panic!("Error connecting  to {}", database_url));
 
+#[debug_handler]
+async fn handler2(State(state): State<AppState>) -> Json<Vec<TodosToPersist>> {
+    let mut connection = state
+        .pool
+        .get()
+        .expect("Failed to get a connection from the pool");
     let vec = TodosToPersist::load(&mut connection);
     Json(vec)
 }
